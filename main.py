1e1e562e544159ff45b8ae41c429889b00f39393
MicahNE <micahnellison@gmail.com>
2023-11-11 10:16:05 -0600

    Updating main.py
    Moved the code for populating the todo  list in the UI into its own class.

--- main.py
+++ main.py
@@ -17,23 +17,25 @@ class Main(QMainWindow):
         super(Main, self).__init__()
         loadUi("main.ui", self)
         self.create_db()
-        
-    #List items     |     This will need to be SQL
-        todos = ["first", "second", "third"]
-        for todo in todos:
-            item = QListWidgetItem(todo)
-            item.setFlags(item.flags() | QtCore.Qt.ItemIsUserCheckable)
-            item.setCheckState(QtCore.Qt.Unchecked)
-            self.todo_listWidget.addItem(item)
-            
+        self.listItems()
+    
         
         
         #Add List Button  
         self.todolistedit_toolButton.clicked.connect(self.addList)
  
         
+        #populating and printing the list information. Needs to be linked to MySQL
+    def listItems(self):
+        todos = ["first", "second", "third"]
+        for todo in todos:
+            item = QListWidgetItem(todo)
+            item.setFlags(item.flags() | QtCore.Qt.ItemIsUserCheckable)
+            item.setCheckState(QtCore.Qt.Unchecked)
+            self.todo_listWidget.addItem(item)
         
-            
+        
+    #event for add button pressed, Needs to add "list" entry to MySQL TodoList table          
     def addList(self):
     
         
@@ -83,6 +85,7 @@ class Main(QMainWindow):
         #     """
         # )
         
+        #this is debug code
         print(forecastDB.tables())
 
         if not forecastDB.open():
@@ -101,59 +104,4 @@ if __name__ == '__main__':
     window = Main()
     window.show()
     app.exec_()
-
-
-
-
-
-
-
-
-
-
-
-# # You need one (and only one) QApplication instance per application.
-# # Pass in sys.argv to allow command line arguments for your app.
-# # If you know you won't use command line arguments QApplication([]) works too.
-# app = QApplication(sys.argv)
-
-# # Create a Qt widget, which will be our window.
-# window = QWidget()
-# window.show()  # IMPORTANT!!!!! Windows are hidden by default.
-# window.setWindowTitle("Forecast")
-# window.setStyleSheet("background: white;")
-
-# grid = QGridLayout()
-# smallgrid = QGridLayout()
-
-# #button widget
-# button = QPushButton("Add Event")
-# button.setCursor(QCursor(QtCore.Qt.PointingHandCursor))
-# button.setStyleSheet("border-radius: 15px")
-
-# #calendar widget
-# calendar = QCalendarWidget()
-
-# #Calendar formatting
-# format = QTextCharFormat()
-# format.setFont(QFont('Times', 15))
-
-
-# # date
-# date = QDate(2020, 6, 10)
- 
-# # setting date text format
-# calendar.setDateTextFormat(date, format)
-# calendar.setVerticalHeaderFormat(0)
-
-# #grid
-# grid.addLayout(smallgrid, 0, 0)
-# grid.addWidget(calendar,0,1)
-
-# smallgrid.addWidget(button,0,0)
-
-
-# window.setLayout(grid)
-# window.show()
-# # Start the event loop.
-# sys.exit(app.exec())
+    
