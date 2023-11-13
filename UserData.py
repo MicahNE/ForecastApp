128708013f46da4e5ed93c72791768f2195c0dc6
Brookelyn Meyer <bmeyer4s@semo.edu>
2023-11-13 16:42:01 -0600

    UserData.py
    barebones UserData.py file

---
+++ UserData.py
@@ -0,0 +1,25 @@
+class UserData:
+    def __init__(self):
+        # class specific variables
+        pass # we will delete pass once we add the code
+
+    def event_start_time(self, date):
+        # logic for handling event start time based on the date parameter
+        pass
+
+    def event_title(self, title):
+        # logic for handling event title based on the title parameter
+        pass
+
+    def update_event(self, data):
+        # logic for updating an event based on the data parameter
+        pass
+
+#  Example usage:
+# if __name__ == "__main__":
+#     user_data_instance = UserData()
+
+#     # Example calls for the methods
+#     start_time = user_data_instance.event_start_time("2023-11-13")
+#     event_title = user_data_instance.event_title("Example Event")
+#     user_data_instance.update_event("Updated Event Data")
