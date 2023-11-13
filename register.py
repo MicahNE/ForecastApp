8f298ce3030986389f333927ea991b558d4b380a
Brookelyn Meyer <bmeyer4s@semo.edu>
2023-11-13 16:26:30 -0600

    register.py
    Added the barebones structure for the register class with the new username and new password functions

---
+++ register.py
@@ -0,0 +1,21 @@
+class Register:
+    def __init__(self):
+        # we will delete pass when adding the actual code
+        pass
+
+    def new_username_input(self, new_username):
+        # new username logic
+        pass
+
+    def new_password_input(self, new_password):
+        # new password input logic
+        pass
+
+
+# Example usage:
+# if __name__ == "__main__":
+#     register_instance = Register()
+
+#     # Example calls
+#     register_instance.new_username_input("new_example_username")
+#     register_instance.new_password_input("new_example_password")
