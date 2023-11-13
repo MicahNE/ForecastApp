0a599ae854607c3af783eabb8be2ed7ae0c3b8cf
Brookelyn Meyer <bmeyer4s@semo.edu>
2023-11-13 16:22:59 -0600

    login.py
    Added the bare bones structure for the login.py

---
+++ login.py
@@ -0,0 +1,23 @@
+
+class Login:
+    def __init__(self):
+        # barebones function
+        pass # we will delete all 'pass' in the code once we add the actual structure to the function
+
+    def username_input(self, username):
+        # username input function
+        pass
+
+    def password_input(self, password):
+        # password input function
+        pass
+
+    def new_user(self):
+        # new user function
+        pass
+
+# Example:
+# username_input("brookelyn_meyer")
+# password_input("secure_password")
+# result = new_user("new_user", "new_password")
+# print(result)
