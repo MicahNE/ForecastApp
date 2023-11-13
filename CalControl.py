a1302485a48333892094e570dc9d95ca553e615d
Brookelyn Meyer <bmeyer4s@semo.edu>
2023-11-13 16:49:58 -0600

    CalControl.py
    barebones calcontrol.py file

---
+++ CalControl.py
@@ -0,0 +1,25 @@
+class CalControl:
+    def __init__(self):
+        # initialize any class specific variables here
+        pass # we will delete 'pass' when we add the actual code
+
+    def has_event(self, has_event_bool):
+        # logic for handling whether an event exists based on the boolean parameter
+        pass
+
+    def event_start_time(self, event_start_time):
+        #  logic for handling the event start time based on the date parameter
+        pass
+
+    def current_time(self, current_time):
+        # logic for handling the current time based on the date parameter
+        pass
+
+# Example usage:
+# if __name__ == "__main__":
+#     cal_control_instance = CalControl()
+
+#     # Example calls for the methods
+#     cal_control_instance.has_event(True)
+#     cal_control_instance.event_start_time("2023-11-13 10:00:00")
+#     cal_control_instance.current_time("2023-11-13 09:30:00")
