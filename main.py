8baa209d97b85a9f52a120739032670e6d164a5e
Brookelyn Meyer <bmeyer4s@semo.edu>
2023-11-13 17:17:29 -0600

    change main.py to have pink accents and be light grey
    change main.py to have pink accents and be light grey

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-
 # Only needed for access to command line arguments
 import sys
 
@@ -17,41 +16,29 @@ class Main(QMainWindow):
         loadUi("main.ui", self)
         self.create_db()
         self.listItems()
-    
-        
-        
-        #Add List Button  
+
+        # Set the background color for the main window
+        self.setStyleSheet("background-color: lightgrey;")
+
+        # Add List Button
         self.todolistedit_toolButton.clicked.connect(self.addList)
- 
-        
-        #populating and printing the list information. Needs to be linked to MySQL
-    def listItems(self):
-        todos = ["first", "second", "third"]
-        for todo in todos:
-            item = QListWidgetItem(todo)
-            item.setFlags(item.flags() | QtCore.Qt.ItemIsUserCheckable)
-            item.setCheckState(QtCore.Qt.Unchecked)
-            self.todo_listWidget.addItem(item)
-        
-        
-    #event for add button pressed, Needs to add "list" entry to MySQL TodoList table          
-    def addList(self):
-    
-        
-        print("pressed")
 
+        # Populating and printing the list information. Needs to be linked to MySQL
+        # ...
 
-    #SQLite DB creation
+    # Add other methods here
+
+    # SQLite DB creation
     def create_db(self):
         forecastDB = QSqlDatabase.addDatabase('QSQLITE')
-        
+
         forecastDB.setDatabaseName('Forecastdb')
-        
+
         forecastDB.open()
-        
-        #Table Creation
+
+        # Table Creation
         createTableQuery = QSqlQuery()
-        
+
         createTableQuery.exec(
             """
             CREATE TABLE Calendar (
@@ -59,9 +46,7 @@ class Main(QMainWindow):
                 date VARCHAR(40) NOT NULL,
                 FOREIGN KEY(date) REFERENCES Event(start)
             )
-                                   
             """
-            
         )
         createTableQuery.exec(
             """
@@ -74,35 +59,44 @@ class Main(QMainWindow):
             )
             """
         )
-        
-        # createTableQuery.exec(
+
+        # CreateTableQuery.exec(
         #     """
         #     CREATE TABLE Todolist (
         #         todoitemid INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE NOT NULL,
-        #         listitem VARCHAR(60) NOT NULL              
+        #         listitem VARCHAR(60) NOT NULL
         #     )
         #     """
         # )
-        
-        #this is debug code
+
+        # This is debug code
         print(forecastDB.tables())
 
         if not forecastDB.open():
             print("Qt failed to open database")
             return False
-        else :
+        else:
             print("Database Connected")
             return True
-        
 
-            
+    def listItems(self):
+        todos = ["first", "second", "third"]
+        for todo in todos:
+            item = QListWidgetItem(todo)
+            item.setFlags(item.flags() | QtCore.Qt.ItemIsUserCheckable)
+            item.setCheckState(QtCore.Qt.Unchecked)
+            self.todo_listWidget.addItem(item)
 
-        
+        # Set the background color and border for the todo_listWidget
+        self.todo_listWidget.setStyleSheet("background-color: lightpink; border: 1px solid pink;")
+
+    # Event for add button pressed, Needs to add "list" entry to MySQL TodoList table
+    def addList(self):
+        print("pressed")
+
+
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     window = Main()
     window.show()
     app.exec_()
-
-    #hello
-    
