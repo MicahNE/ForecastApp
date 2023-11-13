2b2ca21baee7a2efbd8c2876d78d906e4caa5ca4
Brookelyn Meyer <bmeyer4s@semo.edu>
2023-11-13 16:53:30 -0600

    CalDisplay.py
    barebones caldisplay.py file

---
+++ CalDisplay.py
@@ -0,0 +1,65 @@
+class CalDisplay:
+    def __init__(self):
+        # initialize any class specific variables here
+        pass # we will get rid of pass whenever we add the code
+
+    def event_date(self, event_date):
+        # the logic for handling the event date based on the string parameter
+        pass
+
+    def add_edit_event(self, event_info):
+        # the logic for adding or editing an event based on the string parameter
+        pass
+
+    def todo_list(self, todo_list):
+        # the logic for handling the todo list based on the string parameter
+        pass
+
+    def display_register_page(self, new_user):
+        # the logic for displaying the register page based on the newUser parameter
+        pass
+
+    def display_login_page(self):
+        # the logic for displaying the login page
+        pass
+
+    def get_event_data(self):
+        # the logic for retrieving event data
+        pass
+
+    def display_calendar(self):
+        # the logic for displaying the calendar
+        pass
+
+    def save_calendar(self, event_data):
+        # the logic for saving the calendar based on the event data parameter
+        pass
+
+    def update_display(self, event_data):
+        # the logic for updating the display based on the event data parameter
+        pass
+
+    def notifications(self):
+        # the logic for handling notifications
+        pass
+
+    def todo_list(self):
+        #  logic for handling the todo list
+        pass
+
+# Example usage:
+# if __name__ == "__main__":
+#     cal_display_instance = CalDisplay()
+
+    # Example calls for the methods
+    # cal_display_instance.event_date("2023-11-13")
+    # cal_display_instance.add_edit_event("Event details...")
+    # cal_display_instance.todo_list("Todo item 1, Todo item 2")
+    # cal_display_instance.display_register_page(True)
+    # cal_display_instance.display_login_page()
+    # cal_display_instance.get_event_data()
+    # cal_display_instance.display_calendar()
+    # cal_display_instance.save_calendar(event_data)
+    # cal_display_instance.update_display(event_data)
+    # cal_display_instance.notifications()
+    # cal_display_instance.todo_list()
